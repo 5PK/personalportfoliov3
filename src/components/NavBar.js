7d237761cc2ff685b14c37250fb0ec2f880af726
Kevin Tran <k.tran@method.me>
2022-05-05 12:19:20 -0400

    Change some styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ export default function NavBar() {
   return (
     <nav className="z-depth-0">
     <div className="nav-wrapper z-depth-0">
-        <Link to='/' className="brand-logo black-text" style={{paddingLeft: "3%"}}>kevincodes.ca</Link>
+        <Link to='/' className="brand-logo black-text" style={{paddingLeft: "3%"}}>kevincodes</Link>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li><Link to='/experience' className="black-text">experience</Link></li>
             <li><Link to='/projects'className="black-text">personal projects</Link></li>
